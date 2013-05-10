2b48110d45f7aed945b363e9a9526db1e484551c
Simon Carpentier <simon.carpentier@spacebar.ca>
2013-05-09 22:05:40 -0400

    First commit

---
+++ Core/Objects/Track.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SoundCloudPodcast.Core.Objects
+{
+    public class Track
+    {
+        public long Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public long Duration { get; set; }
+        public bool Streamable { get; set; }
+        public bool Downloadable { get; set; }
+        public string Permalink { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string TrackType { get; set; } // TODO: Enum
+        public string OriginalFormat { get; set; }
+        public string PermalinkUrl { get; set; }
+        public string ArtworkUrl { get; set; }
+        public string WaveformUrl { get; set; }
+        public string StreamUrl { get; set; }
+        public string DownloadUrl { get; set; }
+    }
+}
---
+++ Core/SoundCloudApi.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+using SoundCloudPodcast.Core.Objects;
+
+namespace SoundCloudPodcast.Core
+{
+    public class SoundCloudApi
+    {
+        private string Key { get; set; }
+        private const string ApiBaseUrl = "https://api.soundcloud.com";
+
+        public SoundCloudApi(string key)
+        {
+            Key = key;
+        }
+
+        public List<Track> GetTracks(string user)
+        {
+            const string serviceUrl = "/users/{1}/tracks.json?consumer_key={0}";
+
+            var url = string.Format(ApiBaseUrl + serviceUrl, Key, user);
+            var jsonData = new System.Net.WebClient().DownloadString(url);
+
+            return (from t in JArray.Parse(jsonData)
+                    select new Track
+                        {
+                            ArtworkUrl = (string) t["artwork_url"],
+                            CreatedAt = (DateTime) t["created_at"],
+                            Description = (string) t["description"],
+                            Downloadable = (bool) t["downloadable"],
+                            DownloadUrl = string.Format("{0}?consumer_key={1}", t["download_url"], Key),
+                            Duration = (long) t["duration"],
+                            Id = (long) t["id"],
+                            OriginalFormat = (string) t["original_format"],
+                            Permalink = (string) t["permalink"],
+                            PermalinkUrl = (string) t["permalink_url"],
+                            StreamUrl = (string) t["stream_url"],
+                            Streamable = (bool) t["streamable"],
+                            Title = (string) t["title"],
+                            TrackType = (string) t["track_type"],
+                            WaveformUrl = (string) t["waveform_url"]
+                        }).ToList();
+        }
+    }
+}
